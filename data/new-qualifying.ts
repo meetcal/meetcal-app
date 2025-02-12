d256dbdb88450adf48793d3433b82a0f45a8d796
mmohnsen <memohnsen@gmail.com>
2025-02-12 17:18:03 -0500

    nats unchecked

--- data/new-qualifying.ts
+++ data/new-qualifying.ts
@@ -30,106 +30,105 @@ export const qualifyingTotals: QualifyingTotals = {
         {
           name: "Senior",
           weightClasses: [
-            { bodyweightDivision: "55kg", qt: "195" },
-            { bodyweightDivision: "61kg", qt: "210" },
-            { bodyweightDivision: "67kg", qt: "230" },
-            { bodyweightDivision: "73kg", qt: "260" },
-            { bodyweightDivision: "81kg", qt: "274" },
-            { bodyweightDivision: "89kg", qt: "290" },
-            { bodyweightDivision: "96kg", qt: "303" },
-            { bodyweightDivision: "102kg", qt: "305" },
-            { bodyweightDivision: "109kg", qt: "308" },
-            { bodyweightDivision: "+109kg", qt: "311" }
+            { bodyweightDivision: "60kg", qt: "209" },
+            { bodyweightDivision: "65kg", qt: "227" },
+            { bodyweightDivision: "71kg", qt: "256" },
+            { bodyweightDivision: "79kg", qt: "272" },
+            { bodyweightDivision: "88kg", qt: "289" },
+            { bodyweightDivision: "98kg", qt: "305" },
+            { bodyweightDivision: "110kg", qt: "309" },
+            { bodyweightDivision: "110+kg", qt: "312" }
           ]
         },
         {
           name: "U25",
           weightClasses: [
-            { bodyweightDivision: "55kg", qt: "139" },
-            { bodyweightDivision: "61kg", qt: "169" },
-            { bodyweightDivision: "67kg", qt: "182" },
-            { bodyweightDivision: "73kg", qt: "199" },
-            { bodyweightDivision: "81kg", qt: "211" },
-            { bodyweightDivision: "89kg", qt: "221" },
-            { bodyweightDivision: "96kg", qt: "226" },
-            { bodyweightDivision: "102kg", qt: "231" },
-            { bodyweightDivision: "109kg", qt: "234" },
-            { bodyweightDivision: "+109kg", qt: "236" }
+            { bodyweightDivision: "60kg", qt: "168" },
+            { bodyweightDivision: "65kg", qt: "179" },
+            { bodyweightDivision: "71kg", qt: "196" },
+            { bodyweightDivision: "79kg", qt: "208" },
+            { bodyweightDivision: "88kg", qt: "220" },
+            { bodyweightDivision: "98kg", qt: "228" },
+            { bodyweightDivision: "110kg", qt: "235" },
+            { bodyweightDivision: "110+kg", qt: "237" }
           ]
         },
         {
           name: "U23",
           weightClasses: [
-            { bodyweightDivision: "55kg", qt: "137" },
-            { bodyweightDivision: "61kg", qt: "167" },
-            { bodyweightDivision: "67kg", qt: "180" },
-            { bodyweightDivision: "73kg", qt: "197" },
-            { bodyweightDivision: "81kg", qt: "209" },
-            { bodyweightDivision: "89kg", qt: "219" },
-            { bodyweightDivision: "96kg", qt: "224" },
-            { bodyweightDivision: "102kg", qt: "229" },
-            { bodyweightDivision: "109kg", qt: "232" },
-            { bodyweightDivision: "+109kg", qt: "234" }
+            { bodyweightDivision: "60kg", qt: "166" },
+            { bodyweightDivision: "65kg", qt: "177" },
+            { bodyweightDivision: "71kg", qt: "194" },
+            { bodyweightDivision: "79kg", qt: "206" },
+            { bodyweightDivision: "88kg", qt: "218" },
+            { bodyweightDivision: "98kg", qt: "226" },
+            { bodyweightDivision: "110kg", qt: "233" },
+            { bodyweightDivision: "110+kg", qt: "235" }
           ]
         },
         {
           name: "Junior",
           weightClasses: [
-            { bodyweightDivision: "55kg", qt: "135" },
-            { bodyweightDivision: "61kg", qt: "165" },
-            { bodyweightDivision: "67kg", qt: "178" },
-            { bodyweightDivision: "73kg", qt: "195" },
-            { bodyweightDivision: "81kg", qt: "207" },
-            { bodyweightDivision: "89kg", qt: "217" },
-            { bodyweightDivision: "96kg", qt: "222" },
-            { bodyweightDivision: "102kg", qt: "227" },
-            { bodyweightDivision: "109kg", qt: "230" },
-            { bodyweightDivision: "+109kg", qt: "232" }
+            { bodyweightDivision: "60kg", qt: "164" },
+            { bodyweightDivision: "65kg", qt: "175" },
+            { bodyweightDivision: "71kg", qt: "192" },
+            { bodyweightDivision: "79kg", qt: "204" },
+            { bodyweightDivision: "88kg", qt: "216" },
+            { bodyweightDivision: "98kg", qt: "224" },
+            { bodyweightDivision: "110kg", qt: "231" },
+            { bodyweightDivision: "110+kg", qt: "233" }
           ]
         },
         {
           name: "U17",
           weightClasses: [
-            {bodyweightDivision: "49kg", qt: "103"},
-            {bodyweightDivision: "55kg", qt: "115"},
-            {bodyweightDivision: "61kg", qt: "130"},
-            {bodyweightDivision: "67kg", qt: "145"},
-            {bodyweightDivision: "73kg", qt: "158"},
-            {bodyweightDivision: "81kg", qt: "170"},
-            {bodyweightDivision: "89kg", qt: "180"},
-            {bodyweightDivision: "96kg", qt: "190"},
-            {bodyweightDivision: "102kg", qt: "194"},
-            {bodyweightDivision: "+102kg", qt: "198"}
+            { bodyweightDivision: "56kg", qt: "114" },
+            { bodyweightDivision: "60kg", qt: "129" },
+            { bodyweightDivision: "65kg", qt: "142" },
+            { bodyweightDivision: "71kg", qt: "155" },
+            { bodyweightDivision: "79kg", qt: "168" },
+            { bodyweightDivision: "88kg", qt: "178" },
+            { bodyweightDivision: "98kg", qt: "188" },
+            { bodyweightDivision: "98+kg", qt: "191" }
           ]
         },
         {
           name: "U15",
           weightClasses: [
-            {bodyweightDivision: "39kg", qt: "60"},
-            {bodyweightDivision: "44kg", qt: "70"},
-            {bodyweightDivision: "49kg", qt: "80"},
-            {bodyweightDivision: "55kg", qt: "105"},
-            {bodyweightDivision: "61kg", qt: "115"},
-            {bodyweightDivision: "67kg", qt: "120"},
-            {bodyweightDivision: "73kg", qt: "125"},
-            {bodyweightDivision: "81kg", qt: "135"},
-            {bodyweightDivision: "89kg", qt: "141"},
-            {bodyweightDivision: "+89kg", qt: "147"}
+            { bodyweightDivision: "48kg", qt: "76" },
+            { bodyweightDivision: "52kg", qt: "86" },
+            { bodyweightDivision: "56kg", qt: "106" },
+            { bodyweightDivision: "60kg", qt: "113" },
+            { bodyweightDivision: "65kg", qt: "118" },
+            { bodyweightDivision: "71kg", qt: "123" },
+            { bodyweightDivision: "79kg", qt: "133" },
+            { bodyweightDivision: "79+kg", qt: "138" }
           ]
         },
         {
           name: "U13",
           weightClasses: [
-            {bodyweightDivision: "32kg", qt: "32"},
-            {bodyweightDivision: "36kg", qt: "36"},
-            {bodyweightDivision: "39kg", qt: "39"},
-            {bodyweightDivision: "44kg", qt: "44"},
-            {bodyweightDivision: "49kg", qt: "49"},
-            {bodyweightDivision: "55kg", qt: "55"},
-            {bodyweightDivision: "61kg", qt: "61"},
-            {bodyweightDivision: "67kg", qt: "67"},
-            {bodyweightDivision: "73kg", qt: "73"},
-            {bodyweightDivision: "+73kg", qt: "74"},
+            { bodyweightDivision: "40kg", qt: "40" },
+            { bodyweightDivision: "44kg", qt: "44" },
+            { bodyweightDivision: "48kg", qt: "48" },
+            { bodyweightDivision: "52kg", qt: "52" },
+            { bodyweightDivision: "56kg", qt: "56" },
+            { bodyweightDivision: "60kg", qt: "60" },
+            { bodyweightDivision: "65kg", qt: "65" },
+            { bodyweightDivision: "65+kg", qt: "66" }
+          ]
+        },
+        {
+          name: "U11",
+          weightClasses: [
+            { bodyweightDivision: "40kg", qt: "40" },
+            { bodyweightDivision: "44kg", qt: "44" },
+            { bodyweightDivision: "48kg", qt: "48" },
+            { bodyweightDivision: "52kg", qt: "52" },
+            { bodyweightDivision: "56kg", qt: "56" },
+            { bodyweightDivision: "60kg", qt: "60" },
+            { bodyweightDivision: "65kg", qt: "65" },
+            { bodyweightDivision: "65+kg", qt: "66" }
           ]
         }
       ]
@@ -139,106 +138,105 @@ export const qualifyingTotals: QualifyingTotals = {
         {
           name: "Senior",
           weightClasses: [
-            { bodyweightDivision: "45kg", qt: "120" },
-            { bodyweightDivision: "49kg", qt: "137" },
-            { bodyweightDivision: "55kg", qt: "151" },
-            { bodyweightDivision: "59kg", qt: "166" },
-            { bodyweightDivision: "64kg", qt: "177" },
-            { bodyweightDivision: "71kg", qt: "185" },
-            { bodyweightDivision: "76kg", qt: "190" },
-            { bodyweightDivision: "81kg", qt: "193" },
-            { bodyweightDivision: "87kg", qt: "194" },
-            { bodyweightDivision: "+87kg", qt: "195" }
+            { bodyweightDivision: "48kg", qt: "135" },
+            { bodyweightDivision: "53kg", qt: "148" },
+            { bodyweightDivision: "58kg", qt: "164" },
+            { bodyweightDivision: "63kg", qt: "175" },
+            { bodyweightDivision: "69kg", qt: "182" },
+            { bodyweightDivision: "77kg", qt: "191" },
+            { bodyweightDivision: "86kg", qt: "193" },
+            { bodyweightDivision: "86+kg", qt: "195" }
           ]
         },
         {
           name: "U25",
           weightClasses: [
-            { bodyweightDivision: "45kg", qt: "89" },
-            { bodyweightDivision: "49kg", qt: "104" },
-            { bodyweightDivision: "55kg", qt: "121" },
-            { bodyweightDivision: "59kg", qt: "130" },
-            { bodyweightDivision: "64kg", qt: "135" },
-            { bodyweightDivision: "71kg", qt: "138" },
-            { bodyweightDivision: "76kg", qt: "143" },
-            { bodyweightDivision: "81kg", qt: "147" },
-            { bodyweightDivision: "87kg", qt: "152" },
-            { bodyweightDivision: "+87kg", qt: "158" }
+            { bodyweightDivision: "48kg", qt: "103" },
+            { bodyweightDivision: "53kg", qt: "118" },
+            { bodyweightDivision: "58kg", qt: "129" },
+            { bodyweightDivision: "63kg", qt: "134" },
+            { bodyweightDivision: "69kg", qt: "135" },
+            { bodyweightDivision: "77kg", qt: "144" },
+            { bodyweightDivision: "86kg", qt: "151" },
+            { bodyweightDivision: "86+kg", qt: "156" }
           ]
         },
         {
           name: "U23",
           weightClasses: [
-            { bodyweightDivision: "45kg", qt: "87" },
-            { bodyweightDivision: "49kg", qt: "102" },
-            { bodyweightDivision: "55kg", qt: "119" },
-            { bodyweightDivision: "59kg", qt: "128" },
-            { bodyweightDivision: "64kg", qt: "133" },
-            { bodyweightDivision: "71kg", qt: "136" },
-            { bodyweightDivision: "76kg", qt: "141" },
-            { bodyweightDivision: "81kg", qt: "145" },
-            { bodyweightDivision: "87kg", qt: "150" },
-            { bodyweightDivision: "+87kg", qt: "156" }
+            { bodyweightDivision: "48kg", qt: "101" },
+            { bodyweightDivision: "53kg", qt: "116" },
+            { bodyweightDivision: "58kg", qt: "127" },
+            { bodyweightDivision: "63kg", qt: "132" },
+            { bodyweightDivision: "69kg", qt: "133" },
+            { bodyweightDivision: "77kg", qt: "142" },
+            { bodyweightDivision: "86kg", qt: "149" },
+            { bodyweightDivision: "86+kg", qt: "154" }
           ]
         },
         {
           name: "Junior",
           weightClasses: [
-            { bodyweightDivision: "45kg", qt: "85" },
-            { bodyweightDivision: "49kg", qt: "100" },
-            { bodyweightDivision: "55kg", qt: "117" },
-            { bodyweightDivision: "59kg", qt: "126" },
-            { bodyweightDivision: "64kg", qt: "131" },
-            { bodyweightDivision: "71kg", qt: "134" },
-            { bodyweightDivision: "76kg", qt: "139" },
-            { bodyweightDivision: "81kg", qt: "143" },
-            { bodyweightDivision: "87kg", qt: "148" },
-            { bodyweightDivision: "+87kg", qt: "154" }
+            { bodyweightDivision: "48kg", qt: "99" },
+            { bodyweightDivision: "53kg", qt: "114" },
+            { bodyweightDivision: "58kg", qt: "125" },
+            { bodyweightDivision: "63kg", qt: "130" },
+            { bodyweightDivision: "69kg", qt: "131" },
+            { bodyweightDivision: "77kg", qt: "140" },
+            { bodyweightDivision: "86kg", qt: "147" },
+            { bodyweightDivision: "86+kg", qt: "152" }
           ]
         },
         {
           name: "U17",
           weightClasses: [
-            {bodyweightDivision: "40kg", qt: "62"},
-            {bodyweightDivision: "45kg", qt: "69"},
-            {bodyweightDivision: "49kg", qt: "78"},
-            {bodyweightDivision: "55kg", qt: "87"},
-            {bodyweightDivision: "59kg", qt: "98"},
-            {bodyweightDivision: "64kg", qt: "111"},
-            {bodyweightDivision: "71kg", qt: "114"},
-            {bodyweightDivision: "76kg", qt: "117"},
-            {bodyweightDivision: "81kg", qt: "120"},
-            {bodyweightDivision: "+81kg", qt: "122"}
+            { bodyweightDivision: "44kg", qt: "68" },
+            { bodyweightDivision: "48kg", qt: "77" },
+            { bodyweightDivision: "53kg", qt: "84" },
+            { bodyweightDivision: "58kg", qt: "97" },
+            { bodyweightDivision: "63kg", qt: "110" },
+            { bodyweightDivision: "69kg", qt: "112" },
+            { bodyweightDivision: "77kg", qt: "118" },
+            { bodyweightDivision: "77+kg", qt: "121" }
           ]
         },
         {
           name: "U15",
           weightClasses: [
-            {bodyweightDivision: "36kg", qt: "53"},
-            {bodyweightDivision: "40kg", qt: "68"},
-            {bodyweightDivision: "45kg", qt: "72"},
-            {bodyweightDivision: "49kg", qt: "75"},
-            {bodyweightDivision: "55kg", qt: "81"},
-            {bodyweightDivision: "59kg", qt: "87"},
-            {bodyweightDivision: "64kg", qt: "90"},
-            {bodyweightDivision: "71kg", qt: "95"},
-            {bodyweightDivision: "76kg", qt: "98"},
-            {bodyweightDivision: "+76kg", qt: "100"}
+            { bodyweightDivision: "40kg", qt: "68" },
+            { bodyweightDivision: "44kg", qt: "71" },
+            { bodyweightDivision: "48kg", qt: "74" },
+            { bodyweightDivision: "53kg", qt: "79" },
+            { bodyweightDivision: "58kg", qt: "86" },
+            { bodyweightDivision: "63kg", qt: "89" },
+            { bodyweightDivision: "69kg", qt: "93" },
+            { bodyweightDivision: "69+kg", qt: "96" }
           ]
         },
         {
           name: "U13",
           weightClasses: [
-            {bodyweightDivision: "30kg", qt: "30"},
-            {bodyweightDivision: "33kg", qt: "33"},
-            {bodyweightDivision: "36kg", qt: "36"},
-            {bodyweightDivision: "40kg", qt: "40"},
-            {bodyweightDivision: "45kg", qt: "45"},
-            {bodyweightDivision: "49kg", qt: "49"},
-            {bodyweightDivision: "55kg", qt: "55"},
-            {bodyweightDivision: "59kg", qt: "59"},
-            {bodyweightDivision: "64kg", qt: "64"},
-            {bodyweightDivision: "+64kg", qt: "65"}
+            { bodyweightDivision: "36kg", qt: "36" },
+            { bodyweightDivision: "40kg", qt: "40" },
+            { bodyweightDivision: "44kg", qt: "44" },
+            { bodyweightDivision: "48kg", qt: "48" },
+            { bodyweightDivision: "53kg", qt: "53" },
+            { bodyweightDivision: "58kg", qt: "58" },
+            { bodyweightDivision: "63kg", qt: "63" },
+            { bodyweightDivision: "63+kg", qt: "64" }
+          ]
+        },
+        {
+          name: "U11",
+          weightClasses: [
+            { bodyweightDivision: "36kg", qt: "36" },
+            { bodyweightDivision: "40kg", qt: "40" },
+            { bodyweightDivision: "44kg", qt: "44" },
+            { bodyweightDivision: "48kg", qt: "48" },
+            { bodyweightDivision: "53kg", qt: "53" },
+            { bodyweightDivision: "58kg", qt: "58" },
+            { bodyweightDivision: "63kg", qt: "63" },
+            { bodyweightDivision: "63+kg", qt: "64" }
           ]
         },
       ]
