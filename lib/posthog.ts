946cd5d212c5fbf43d2a26fb4eb920cf8a55444b
Maddisen Mohnsen <memohnsen@gmail.com>
2025-03-29 11:17:56 -0400

    post vers

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,4 +1,5 @@
 import PostHog from 'posthog-react-native';
+import Constants from 'expo-constants';
 
 // Initialize PostHog
 const posthog = new PostHog(
@@ -7,5 +8,10 @@ const posthog = new PostHog(
     host: process.env.EXPO_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
   }
 );
+
+// Set app version
+posthog.capture('$app_version', {
+  version: Constants.expoConfig?.version || 'unknown'
+});
 
 export { posthog }; 
