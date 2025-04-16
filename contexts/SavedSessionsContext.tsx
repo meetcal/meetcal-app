abc25a5a3c99479da90e7e1e17c52d40189602ab
Maddisen Mohnsen <memohnsen@gmail.com>
2025-04-16 17:34:38 -0400

    reset clears the db and local for selected meet

--- contexts/SavedSessionsContext.tsx
+++ contexts/SavedSessionsContext.tsx
@@ -9,7 +9,7 @@ interface SavedSessionsContextType {
   saveSession: (session: SavedSession) => Promise<boolean>;
   removeSession: (sessionId: string) => Promise<boolean>;
   isSessionSaved: (sessionId: string) => boolean;
-  resetAllSessions: () => Promise<boolean>;
+  resetAllSessions: (meet?: MeetName) => Promise<boolean>;
 }
 
 const SavedSessionsContext = createContext<SavedSessionsContextType | undefined>(undefined);
