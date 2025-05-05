a8fe3f5cc40ecaa16486f92692c187fcce282a83
Maddisen Mohnsen <memohnsen@gmail.com>
2025-05-05 17:33:44 -0400

    not found now a loading inbetween state page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, View, ActivityIndicator } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -7,11 +8,23 @@ import { ThemedView } from '@/components/ThemedView';
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: 'Loading...' }} />
       <ThemedView style={styles.container}>
-        <Link href="/schedule" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
+        <Image
+          source={require('@/assets/images/MeetCal-no-bg.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <ActivityIndicator size="large" style={styles.spinner} />
+        <ThemedText type="title" style={styles.text}>
+          Heading to your page now
+        </ThemedText>
+        <ThemedText style={styles.subtext}>
+          If the page doesn't automatically load,{' '}
+          <Link href="/schedule" style={styles.link}>
+            <ThemedText type="link">click here</ThemedText>
+          </Link>
+        </ThemedText>
       </ThemedView>
     </>
   );
@@ -24,8 +37,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 20,
   },
+  logo: {
+    width: 120,
+    height: 60,
+    marginBottom: 32,
+  },
+  spinner: {
+    marginBottom: 24,
+  },
+  text: {
+    textAlign: 'center',
+    marginBottom: 12,
+  },
+  subtext: {
+    textAlign: 'center',
+    color: '#888',
+  },
   link: {
-    marginTop: 15,
-    paddingVertical: 15,
+    marginLeft: 4,
   },
 });
