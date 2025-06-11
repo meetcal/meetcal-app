fc7f1d9b7c439cd67d2363075d04137c0f635c53
Maddisen Mohnsen <memohnsen@gmail.com>
2025-06-11 17:44:37 -0400

    here we go

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,14 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 // Configure path aliases
 config.resolver.alias = {
-  '@': __dirname,
+  '@': path.resolve(__dirname, '.'),
 };
+
+// Ensure we include the root directory in module resolution
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
 
 module.exports = config; 
