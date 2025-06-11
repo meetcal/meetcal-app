534f1667ffe56f0f70699bc1e10a8a9f0fa37607
Maddisen Mohnsen <memohnsen@gmail.com>
2025-06-11 17:32:11 -0400

    try again

---
+++ metro.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+const { transformer, resolver } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
+  sourceExts: [...resolver.sourceExts, 'svg'],
+  alias: {
+    '@': path.resolve(__dirname, './'),
+  },
+};
+
+module.exports = config; 
