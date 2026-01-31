3655ebf9a5a76239ead5000f60d509aa58988702
Maddisen Mohnsen <memohnsen@gmail.com>
2026-01-31 16:53:46 -0500

    start list imrpovements

--- app/(tabs)/(index)/_layout.tsx
+++ app/(tabs)/(index)/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from 'expo-router';
+import { Colors } from '@/constants/Colors';
+import { useTheme } from '@/contexts/ThemeContext';
 
 export default function IndexLayout() {
-  return <Stack screenOptions={{ headerStyle: { backgroundColor: 'black' } }}/>;
+  return <Stack screenOptions={{ headerStyle: { backgroundColor: useTheme().currentTheme === 'dark' ? Colors.dark.background : Colors.light.background } }}/>;
 }
