fddf18f74c81cbc4ede371fb561f3ab8b751ef81
Maddisen Mohnsen <memohnsen@gmail.com>
2026-01-31 16:53:46 -0500

    start list imrpovements

--- app/(tabs)/(saved)/_layout.tsx
+++ app/(tabs)/(saved)/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from 'expo-router';
+import { useTheme } from '@/contexts/ThemeContext';
+import { Colors } from '@/constants/Colors';
 
 export default function SavedLayout() {
-  return <Stack screenOptions={{ title: 'Saved', headerStyle: { backgroundColor: 'black' } }} />;
+  return <Stack screenOptions={{ title: 'Saved', headerStyle: { backgroundColor: useTheme().currentTheme === 'dark' ? Colors.dark.background : Colors.light.background } }} />;
 }
